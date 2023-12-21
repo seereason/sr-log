e3149d1068b271e45b82d8eae740cd7ba16bfbea
David Fox <dsf@seereason.com>
2023-12-21 11:08:09 -0800

    * Fix code to drop sr-log stack frames * Add a test suite

--- src/SeeReason/SrcLoc.hs
+++ src/SeeReason/SrcLoc.hs
@@ -16,6 +16,7 @@ module SeeReason.SrcLoc
   ( getStack
   , dropThisPackageFrames
   -- * Full stack formatting
+  , prettyLoc
   , prettyLocN
   , locs
   -- * Compact stack formatting
@@ -28,7 +29,7 @@ module SeeReason.SrcLoc
   , compactStack
   -- * Log string formatting
   , locDrop
-  , tests, testloc, testlocs, teststack
+  -- , tests, testloc, testlocs, teststack
   ) where
 
 import Control.Lens(ix, preview, to)
@@ -37,12 +38,7 @@ import Data.List (intercalate, intersperse, isPrefixOf)
 import Data.Semigroup (Semigroup((<>)))
 #endif
 import Data.String (IsString(fromString))
-#if MIN_VERSION_time(1,9,0)
-import Data.Time.Format (formatTime, defaultTimeLocale)
-#endif
 import GHC.Stack (CallStack, callStack, getCallStack, HasCallStack, SrcLoc(..))
-import GHC.Stack.Types (CallStack(..))
-import Test.HUnit
 
 mintercalate :: Monoid s => s -> [s] -> s
 mintercalate x xs = mconcat (intersperse x xs)
@@ -91,10 +87,10 @@ srcfunloccol :: (IsString s, Semigroup s) => SrcLoc -> s -> s
 srcfunloccol loc f = srcfunloc loc f <> ":" <> fromString (show (srcLocStartLine loc))
 
 thisPackage :: String
-thisPackage = "Extra.SrcLoc"
+thisPackage = "sr-log-"
 
 dropThisPackageFrames :: [(String, SrcLoc)] -> [(String, SrcLoc)]
-dropThisPackageFrames = dropWhile (isPrefixOf thisPackage . srcLocModule . snd)
+dropThisPackageFrames = dropWhile (isPrefixOf thisPackage . srcLocPackage . snd)
 
 -- | Get the portion of the stack before we entered any SeeReason.Log module.
 getStack :: HasCallStack => [(String, SrcLoc)]
@@ -124,90 +120,3 @@ compactStack ((_, loc) : more@((caller, _) : _)) =
     -- figure out which caller is missing the HasCallStack constraint.
     stacktail [loc'] = [srcloccol loc']
     stacktail (loc' : more') = srcloc loc' : stacktail more'
-
--- | more documentation than tests
-tests :: IO ()
-tests =
-  runTestTT cases >>= putStrLn . show
-  where
-    cases :: Test
-    cases =
-      TestList
-      [ TestCase (assertEqual "prettyLoc"
-                   "Data.Map:20"
-                   (prettyLoc testloc))
-      , TestCase (assertEqual "srcloc"
-                   "Data.Map:20"
-                   (srcloc @String testloc))
-      , TestCase (assertEqual "srclocs"
-                   ("Main:44 \8594" <>
-                    "AppraisalClient.AppraisalClient:90 \8594" <>
-                    "AppraisalClient.AppraisalClient:131 \8594" <>
-                    "AppraisalBase.Layout.Viewport:61")
-                   (srclocs @String teststack))
-      , TestCase (assertEqual "srcloccol"
-                   "Data.Map:20:1"
-                   (srcloccol @String testloc))
-      , TestCase (assertEqual "srclocList"
-                   ["Main:44",
-                    "AppraisalClient.AppraisalClient:90",
-                    "AppraisalClient.AppraisalClient:131",
-                    "AppraisalBase.Layout.Viewport:61"]
-                   (srclocList @String teststack))
-      , TestCase (assertEqual "srcfunloc"
-                   "Data.Map.foo:20"
-                   (srcfunloc @String testloc "foo"))
-      , TestCase (assertEqual "srcfunloccol"
-                   "Data.Map.foo:20"
-                   (srcfunloc @String testloc "foo"))
-      , TestCase (assertEqual "compactStack"
-                   ("AppraisalBase.Layout.Viewport.layoutViewport:61 \8592 " <>
-                    "AppraisalClient.AppraisalClient:131 \8592 " <>
-                    "AppraisalClient.AppraisalClient:90 \8592 " <>
-                    "Main:44:3")
-                   (compactStack @String testlocs))
-      , TestCase (assertEqual "locs 0"
-                   ("AppraisalBase.Layout.Viewport:61 -> " <>
-                    "AppraisalClient.AppraisalClient:131 -> " <>
-                    "AppraisalClient.AppraisalClient:90 -> Main:44")
-                   (locs teststack 0))
-      , TestCase (assertEqual "locs 2"
-                   "AppraisalClient.AppraisalClient:90 -> Main:44"
-                   (locs teststack 2))
-      , TestCase (assertEqual "prettyLocN 2"
-                   (Just "AppraisalClient.AppraisalClient:90")
-                   (prettyLocN teststack 2))
-
-      ]
-
-testloc :: SrcLoc
-testloc = SrcLoc "containers" "Data.Map" "<file>" 20 1 21 10
-
-teststack :: CallStack
-teststack = f testlocs
-  where f :: [(String, SrcLoc)] -> CallStack
-        f [] = EmptyCallStack
-        f ((name, loc) : more) = PushCallStack name loc (f more)
-
-testlocs :: [(String, SrcLoc)]
-testlocs =
-  [("viewportSkeleton",
-    SrcLoc {srcLocPackage = "happstack-ghcjs-common-0.1.3-inplace",
-            srcLocModule = "AppraisalBase.Layout.Viewport",
-            srcLocFile = "src/AppraisalBase/Layout/Viewport.hs",
-            srcLocStartLine = 61, srcLocStartCol = 9, srcLocEndLine = 61, srcLocEndCol = 60}),
-   ("layoutViewport",
-    SrcLoc {srcLocPackage = "happstack-ghcjs-common-0.1.3-inplace",
-            srcLocModule = "AppraisalClient.AppraisalClient",
-            srcLocFile = "src/AppraisalClient/AppraisalClient.hs",
-            srcLocStartLine = 131, srcLocStartCol = 20, srcLocEndLine = 131, srcLocEndCol = 40}),
-   ("appraisalUI",
-    SrcLoc {srcLocPackage = "happstack-ghcjs-common-0.1.3-inplace",
-            srcLocModule = "AppraisalClient.AppraisalClient",
-            srcLocFile = "src/AppraisalClient/AppraisalClient.hs",
-            srcLocStartLine = 90, srcLocStartCol = 32, srcLocEndLine = 90, srcLocEndCol = 43}),
-   ("clientMain",
-    SrcLoc {srcLocPackage = "main",
-            srcLocModule = "Main",
-            srcLocFile = "src/Main.hs",
-            srcLocStartLine = 44, srcLocStartCol = 3, srcLocEndLine = 44, srcLocEndCol = 72})]
